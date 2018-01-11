9233705bafae5c46b41c375e12efe37f435c99f0
wunaifu <1278022159@qq.com>
2018-01-11 22:39:49 +0800

    Update UserController.java

--- helpServer/helpserver-manager-web/src/main/java/com/helpserver/controller/UserController.java
+++ helpServer/helpserver-manager-web/src/main/java/com/helpserver/controller/UserController.java
@@ -7,12 +7,14 @@ import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 /**
  * Created by wunaifu on 2018/1/11.
+ ninsdfakajsdf
  */
 @Controller
 public class UserController {
     @RequestMapping("/index")
     public String showIndex() {
         log.println("nihao");
+        //俺的沙发金口难开
         return "index";
     }
 }
